149c72ec08f1058c50bd78f9a05f3281179e69b6
Alissin82 <aliansaipoor82@gmail.com>
2023-03-13 23:04:21 +0330

    refactor

---
+++ Project/php/server.php
@@ -0,0 +1,17 @@
+<?php 
+function connect(){
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "dblab";
+
+  $connection = mysqli_connect($servername,$username,$password,$database);
+  if (!$connection) {
+    die("<p class='alert alert-danger'><strong>اخطار</strong> اتصال به پایگاه داده ناموفق بود</p>");
+  }
+  //$old_connection->set_charset('utf8');
+  mysqli_set_charset($connection,"utf8");
+
+  return $connection;
+  }
+?>
